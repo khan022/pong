5a2dc9f781c523671a35ef9c8f8a867455fe561f
Shafkat Khan Siam <sk.siam@outlook.com>
2023-08-12 16:28:29 +0600

    rectangle update

--- main.lua
+++ main.lua
@@ -25,14 +25,20 @@ function love.load()
     -- }) basic window render version 0.0
 
     -- Filter changing to nearest
-
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- changing the font to our chosen font
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    love.graphics.setFont(smallFont)
+
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,{
         fullscreen = false,
         resizable = false,
         vsync = true
     })
+
 end
 
 
@@ -41,11 +47,13 @@ end
 ]]
 
 function love.keypressed(key)
+
     --keys can be accessed by string name
     if key == 'escape' then
         --funtion to terminate the application
         love.event.quit()
     end
+
 end
 
 --[[
@@ -53,12 +61,29 @@ end
 ]]
 
 function love.draw()
+    
     -- begin rendering at virtual resolution
     push:apply('start')
 
-    love.graphics.printf('Hello Pong!!!', 0, VIRTUAL_HEIGHT/2 - 6, VIRTUAL_WIDTH, 'center') 
+    -- clear the screen with a specific color
+    love.graphics.clear(40/255, 45/255, 52/255, 50/255)
+    
+    -- love.graphics.printf('Hello Pong!!!', 0, VIRTUAL_HEIGHT/2 - 6, VIRTUAL_WIDTH, 'center') 
     -- version 0.0
+
+    love.graphics.printf('Hello Pong!!!', 0, 20, VIRTUAL_WIDTH, 'center')
+    -- version 0.2
+
+    -- render the left paddle
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+    -- render the right paddle
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    -- render the ball at center
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     -- end rendering at virtual resolution
     push:apply('end')
+    
 end
