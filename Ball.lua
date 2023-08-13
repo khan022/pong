3fa338dc2213758ad7dca3df1f4204088dd993fa
Shafkat Khan Siam <sk.siam@outlook.com>
2023-08-13 15:41:59 +0600

    ball and paddle change to class v0.5

--- Ball.lua
+++ Ball.lua
@@ -9,7 +9,7 @@ function Ball:init(x, y, width, height)
     self.height = height
 
     -- these variables keep track of the velocity of the ball in x, y axises.
-    self.dy = math.random(2) == 1 and 100 or -100
+    self.dy = math.random(2) == 1 and -100 or 100
     self.dx = math.random(-50, 50)
 
 end
@@ -22,7 +22,7 @@ function Ball:reset()
 
     self.x = VIRTUAL_WIDTH / 2 - 2
     self.y = VIRTUAL_HEIGHT / 2 - 2
-    self.dy = math.random(2) == 1 and 100 or -100
+    self.dy = math.random(2) == 1 and -100 or 100
     self.dx = math.random(-50, 50)
 
 end
