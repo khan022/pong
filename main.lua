06e49330614ad9eca59531412eb120f5b0e9d58d
Shafkat Khan Siam <sk.siam@outlook.com>
2023-08-14 12:58:42 +0600

    adding sound effects v1.10

--- main.lua
+++ main.lua
@@ -58,6 +58,14 @@ function love.load()
 
     love.graphics.setFont(smallFont)
 
+    -- applying small sounds like hitting the paddle or scores.
+    -- calling these entries from the table to play the sounds.
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+    }
+
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,{
         fullscreen = false,
@@ -128,6 +136,7 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+            sounds['paddle_hit']:play()
         end
         
         if ball:collides(player2) then
@@ -140,24 +149,28 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+            sounds['paddle_hit']:play()
         end
 
         -- detect the upper and lower screen and reverse if collision happen
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
 
         -- now accounting the 4 of the ball size at the lower screen
         if ball.y >= VIRTUAL_HEIGHT - 4 then
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
 
         -- check if the ball is moving out of left or right screen to count score
         if ball.x < 0 then
             servingPlayer = 1
             player2Score = player2Score + 1
+            sounds['score']:play()
 
             -- check if the score is 10 or not
             if player2Score == 10 then
@@ -172,6 +185,7 @@ function love.update(dt)
         if ball.x > VIRTUAL_WIDTH then
             servingPlayer = 2
             player1Score = player1Score + 1
+            sounds['score']:play()
 
             -- check if the score is 10 or not
             if player1Score == 10 then
